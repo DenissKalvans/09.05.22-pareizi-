4e46bebaee511f6e96227bedd4bcf225f93aee97
Denis Kalvan <95209747+DenissKalvans@users.noreply.github.com>
2022-05-09 06:01:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+skaut1=int(input("Ievadi 1.šķautnes garumu: "))
+skaut2=int(input("Ievadi 2.šķautnes garumu: "))
+if skaut1>skaut2:
+  print("1. kuba tilpums: ", skaut1**3)
+elif skaut1<skaut2:
+  print("2. kuba tilpums: ", skaut2**3)
+else:
+  print("Kubi ir vienādi!")
